b7ad37f3aff4e324ec7d8d952e0af76177173d43
rkfs3134 <nejib.abb@gmail.com>
2021-10-08 16:22:54 +0100

    init app

---
+++ src/main/java/na/annotaion/AnnotaionApplication.java
@@ -0,0 +1,13 @@
+package na.annotaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotaionApplication.class, args);
+	}
+
+}
